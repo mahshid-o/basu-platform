7550857246d89fb7791d6d1d17288cba88797124
mahshid <zahra.oveisii@gmail.com>
2024-02-16 12:38:42 +0330

    start implement customusermodel

--- .history/base/models_20240216123547.py
+++ .history/base/models_20240216123547.py
@@ -1,8 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    pass
+# Create your models here.
 
 # class Topic(models.Model):
 #     name = models.CharField(max_length=200)
--- .history/base/models_20240216123727.py
+++ .history/base/models_20240216123727.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
-    pass
+class User
 
 # class Topic(models.Model):
 #     name = models.CharField(max_length=200)
