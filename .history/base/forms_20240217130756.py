5f2b03eaf1ac1cbe1b9b206e27a9ddd4d9b00ef4
mahshid <zahra.oveisii@gmail.com>
2024-02-20 13:16:28 +0330

    changing links of avatar images

---
+++ .history/base/forms_20240217130756.py
@@ -0,0 +1,28 @@
+from django.forms import ModelForm
+from .models import Room , User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class MyUserCreationForm(User):
+    
+
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
+
+
+
+class RoomForm(ModelForm):
+    class Meta:
+        model = Room
+        fields='__all__'
+        exclude = ['host' , 'participants']
+        
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['avatar' ,'name' ,'username' , 'email' , 'bio']
+
+
