4cdb58632add58d200865b1602616cca725fd691
mahshid <zahra.oveisii@gmail.com>
2024-02-20 15:25:09 +0330

    change login and sign up language to persian

--- base/models.py
+++ base/models.py
@@ -1,18 +1,19 @@
+from tabnanny import verbose
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    name = models.CharField(max_length=200,null=True)
-    email = models.EmailField(unique=True , null=True)
-    bio = models.TextField(null=True)
+    name = models.CharField("نام",max_length=200,null=True)
+    email = models.EmailField("ایمیل",unique=True , null=True)
+    bio = models.TextField("بیوگرافی",null=True)
     
-    avatar = models.ImageField(null=True,default="avatar.svg")
+    avatar = models.ImageField("آواتار",null=True,default="avatar.svg")
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
 class Topic(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField("موضوع",max_length=200)
 
     def __str__(self):
         return self.name
