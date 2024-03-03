ef60369176217fd7b48e1379b34092c9a20f6426
mahshid <zahra.oveisii@gmail.com>
2024-03-03 11:22:04 +0330

    customized admin panel

--- base/admin.py
+++ base/admin.py
@@ -1,10 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
-
 from .models import Room,Topic,Message,User
 
-admin.site.register(User)
-admin.site.register(Room)
-admin.site.register(Topic)
-admin.site.register(Message)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["name", "email",]
+    list_display_links = ["name", "email",]
+    empty_value_display = "unknown"
+    search_fields = ["name", "email", "username"]
+    list_filter = ["is_superuser","is_staff","is_active"]
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("host", "topic", "name")
+    list_display_links = ("host", "topic", "name")
+    ordering = ["created"]
+    search_fields = ("name", "description")
+    list_filter = ("created","updated")
+
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    list_display_links = ["name"]
+    search_fields = ["name"]
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("user", "room", "body")
+    list_display_links = ("user", "room")
+    ordering = ["created"]
+    search_fields = ["body"]
+    list_filter = ("created","updated")
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Message, MessageAdmin)
